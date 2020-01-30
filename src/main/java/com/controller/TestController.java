ac70948118cd77a70cae1a0883c4945e1c5420e2
Prashant <prashantkaushik11.pk@gmail.com>
2020-01-31 01:33:41 +0530

    initial commit

---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value="/home")
+    public String test(){
+        return "home";
+    }
+}
